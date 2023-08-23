2f3f15f454c053722d602e18c81ca5a53d28d39b
Dylan Hackworth <dylhack@gmail.com>
2023-08-23 11:48:17 -0500

    #13: Add Standalone Version (#17)

--- cowbot/src/bot/events/ready.rs
+++ cowbot/src/bot/events/ready.rs
@@ -1,10 +1,10 @@
 use super::super::commands::register_all;
 use serenity::{model::gateway::Ready, prelude::Context as SerenityContext};
 
-pub async fn handle(ctx: &SerenityContext, _ready: &Ready) {
+pub async fn handle(ctx: &SerenityContext, rdy: &Ready) {
     if let Err(why) = register_all(ctx).await {
         println!("Error registering commands: {}", why);
     }
 
-    println!("Ready!")
+    println!("Ready as {}!", rdy.user.tag());
 }
